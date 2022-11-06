5f6a683355d289dc5278913eaf02b3f29de82693
Mir Habeel Ahmad <102403483+Habeel06@users.noreply.github.com>
2022-11-06 15:01:13 +0530

    Update cmatrixripoff.py

--- cmatrixripoff.py
+++ cmatrixripoff.py
@@ -12,13 +12,14 @@ print(colored('''
 ██║     ██║╚██╔╝██║██╔══██║   ██║   ██╔══██╗██║ ██╔██╗     ██╔══██╗██║██╔═══╝ ╚════╝██║   ██║██╔══╝  ██╔══╝  
 ╚██████╗██║ ╚═╝ ██║██║  ██║   ██║   ██║  ██║██║██╔╝ ██╗    ██║  ██║██║██║           ╚██████╔╝██║     ██║     
  ╚═════╝╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝╚═╝            ╚═════╝ ╚═╝     ╚═╝     
-                                                                                                             ''', 'green', 'on_red'))
+                                                                                                             ''', 'yellow'))
 
-time.sleep(5)                                                                                        
+print(colored('WAIT 3 SECONDS!.', 'cyan'))                                                                                                            
+
+time.sleep(3)                                                                                        
 while True:
-    print(Fore.GREEN + "hi")
-    print(Fore.GREEN + "hi")
-    print(Fore.GREEN + "hi")
-    print(Fore.GREEN + "hi")
-    print(Fore.GREEN + "hi")
-
+    print(Fore.GREEN + " b  5  6  3  2 %  @  !  $  %  *  (  g  K  L  s $  %  #  *  ^  <  >  :  r  5  8  (  %  3  #  ^  b  @  $  d  r  m  %  &  j  _  J  w  @  #  s  a  h  j  g  ^  5  3  @  v  ^ !  e  3  4  $  5  r  f  b   b  5  6  3  2 %  @  !  $  %  *  (  g  K ")
+    time.sleep(0.1)  
+    print(Fore.GREEN + "   6  ^  %  #  j  h  &  )  @  #  s  a  h  j  g  ^  5  3   b  5  6  3  2 %  @  !  $  %  *  (  g  K  L  s $  %  #  *  ^  <  >  :  r  5  8  (  %  3  #  ^  b  @  $  d  r  m  %  &  j  _  J  w  @  #  s  a  h  j  g  ^  5  3   @  $  d  r  m  %   ")
+    time.sleep(0.1)  
+    print(Fore.GREEN + " @  $  d  r  m  %  &  j  _  J  w  @  #  s  a  h  j  g  ^  5  3  @  v  ^ !  e  3  4  $  5  r  f  b   b  5  6  3  2  6  7  *  (  g  K  L  s $  %  #  *  ^ %  &  j  a  h  j  g  ^  5  3  @  v  ^ !  e  3  4  $  5  r  f  b   b  5  6  3  2 %  @  ")
